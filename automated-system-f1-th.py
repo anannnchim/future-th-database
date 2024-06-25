72542db4c3b31c405142be5f68feaf6678d9e0b1
nanthawat <u7540836@anu.edu.au>
2024-06-25 16:53:47 +0700

    add engine

--- automated-system-f1-th.py
+++ automated-system-f1-th.py
@@ -227,7 +227,7 @@ for symbol in holding_information['current_symbol']:
         
         # Store data 
         prev_backadj_df['date'] = prev_backadj_df['date'].dt.strftime('%Y-%m-%d')
-        prev_backadj_df.to_parquet(ticker + '.parquet')
+        prev_backadj_df.to_parquet(ticker + '.parquet',  engine='pyarrow')
     
     else:
         
@@ -266,7 +266,7 @@ for symbol in holding_information['current_symbol']:
         print("3 - Finish: Update googlesheet")
         
         # Save data
-        prev_backadj_df.to_parquet(ticker + '.parquet')
+        prev_backadj_df.to_parquet(ticker + '.parquet', engine='pyarrow')
 
     print("4 - All done.")     
         
