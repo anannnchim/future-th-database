864c88c83258cbc6949d7172df95124ec7fb2724
nanthawat <u7540836@anu.edu.au>
2024-06-25 10:02:00 +0700

    add data

---
+++ test_automation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 09:57:05 2024
+
+@author: nanthawat
+"""
+
+# import library
+
+# Load data from googlesheet
+
+
+# Scrape data from website
+
+
+# Store it 
+
