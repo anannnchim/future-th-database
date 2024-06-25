9171577721a1b9d71ebb5b071145dc70d2b9059c
nanthawat <u7540836@anu.edu.au>
2024-06-25 15:41:10 +0700

    modify name

--- test_google_credentials.py
+++ test_google_credentials.py
@@ -144,7 +144,7 @@ def test_google_credentials():
         print("Retrieved GOOGLE_APPLICATION_CREDENTIALS:", SERVICE_ACCOUNT_FILE)
         print("this is gc: " , gc)
         raw_df = scrape_from_tfex('S50U24') # Slow part 
-        raw_df.to_parquet('data/raw_df' + '.parquet',  engine='pyarrow')
+        raw_df.to_parquet('raw_df.parquet',  engine='pyarrow')
 
         print(raw_df.head())
     else:
